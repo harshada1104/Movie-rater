4aae01caa24b903f8cf27a89d126e1153e85bf3d
Your Name <you@example.com>
2023-03-25 23:04:41 +0530

    initalcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { MovieComponent } from './movie/movie.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FeatureModule } from './feature/feature.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MovieComponent,HomeComponent,HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    NgbModule,
+    FeatureModule,
+    
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  ngOnInit(): void {
+
+  }
+
+  constructor(private router:Router , private auth :AuthService){}
+  goToHome(){
+this.router.navigate(['home'])
+  }
+
+  logout(){
+this.auth.logout();
+  }
+
+}
